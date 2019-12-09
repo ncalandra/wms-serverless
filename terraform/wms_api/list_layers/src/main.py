0511e8c6f24e8745ffc43cbacb378c3d7873316c
Nathan Calandra <njcalandra@gmail.com>
2019-12-09 17:00:40 -0500

    Fix logic to list an S3 bucket contents

--- terraform/wms_api/list_layers/src/main.py
+++ terraform/wms_api/list_layers/src/main.py
@@ -20,8 +20,7 @@ client = boto3.client("s3")
 
 def handler(event, context):
     """AWS Lambda handler."""
-    # TODO: add check if there are more than 1000 layers
-    response = client.list_objects_v2(Bucket=SOURCE_BUCKET, MaxKeys=1000,)
+    layers = list_bucket(SOURCE_BUCKET, "", continuation_token=None, objects=None)
 
     return {
         "statusCode": 200,
@@ -31,11 +30,51 @@ def handler(event, context):
             "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Origin": "*",
         },
-        "body": json.dumps(
+        "body": json.dumps(layers),
+    }
+
+
+def list_bucket(bucket, prefix, continuation_token=None, objects=None):
+    """
+    Recursively lists contents on an S3 bucket.
+
+    Args:
+        bucket: S3 Bucket Name
+        prefix: Prefix to use for objects
+        continuation_token: used for pagination
+        objects: running list of objects
+    Returns:
+        list of objects in the bucket with the specified prefix
+
+    """
+    if objects is None:
+        objects = []
+
+    if not continuation_token:
+        response = client.list_objects_v2(Bucket=bucket, MaxKeys=1000, Prefix=prefix)
+    else:
+        response = client.list_objects_v2(
+            Bucket=bucket,
+            MaxKeys=1000,
+            Prefix=prefix,
+            ContinuationToken=continuation_token,
+        )
+
+    if "Contents" in response:
+        objects.extend(
             [
                 s3_object["Key"]
                 for s3_object in response["Contents"]
                 if ".tif" in s3_object["Key"]
             ]
-        ),
-    }
+        )
+
+    if "NextContinuationToken" in response:
+        return list_bucket(
+            bucket,
+            prefix,
+            continuation_token=response["NextContinuationToken"],
+            objects=objects,
+        )
+
+    return objects
